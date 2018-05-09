d0a1cae7903dbc460e1a5582b091b9ac207c4f1e
dcvan <dcvan@cs.unc.edu>
2018-05-09 13:58:27 -0400

    Add getResourceMetadata endpoint

--- server/controllers/default_controller.py
+++ server/controllers/default_controller.py
@@ -1,8 +1,10 @@
 import sys
+
 from controllers.irods_session import create_session
 from configparser import ConfigParser
 from irods.models import Collection, DataObject, Resource, ResourceMeta
 from irods.column import Criterion
+from irods.exception import CollectionDoesNotExist, DataObjectDoesNotExist
 from flask import jsonify
 
 parser = ConfigParser()
@@ -17,12 +19,6 @@ def get_host_node_get(resource_name) -> str:
         Resource.name == resource_name
     ]
     results = sess.query(Resource.name, Resource.location).filter(*conditions).all()
-    # resc_cond = [
-    #     ResourceMeta.name == 'region',
-    #     ResourceMeta.value == 'us-east-1d'
-    # ]
-    # res = sess.query(Resource.name, Resource.location, ResourceMeta.name, ResourceMeta.value).filter(*resc_cond).all()
-    # print(res)
     sess.cleanup()
     data = {}
     data['hostnode'] = []
@@ -47,6 +43,24 @@ def find_resources_by_metadata(meta_key=None, meta_value=None) -> str:
                         ResourceMeta.value).filter(*conditions).all()
     session.cleanup()
     return jsonify(list(set([r[Resource.name] for r in res.rows])))
+
+
+def get_resource_metadata(resource_name) -> str:
+    sess = create_session()
+    conditions = [
+        Resource.name == resource_name
+    ]
+    res = sess.query(Resource.name,
+                     Resource.location,
+                     ResourceMeta.name,
+                     ResourceMeta.value).filter(*conditions).all()
+    sess.cleanup()
+    if not res.rows:
+        return jsonify(dict(error='Resource not found: %s'%resource_name)), 404
+    resp = {r[ResourceMeta.name]: r[ResourceMeta.value] for r in res.rows}
+    resp.update(name=res.rows[0][Resource.name],
+                host=res.rows[0][Resource.location])
+    return jsonify(resp)
 
 
 def get_host_site_get(resource_name) -> str:
@@ -91,18 +105,21 @@ def get_logical_location_get(filename, match_exact = None, include_trash = None)
 
 
 def get_replicas_get(filename) -> str:
-    sess = create_session()
-    obj = sess.data_objects.get(filename)
-    sess.cleanup()
-    data = {}
-    data['replicas'] = []
-    for replica in obj.replicas:
-        data['replicas'] += [{
-            'resource_name': str(replica.resource_name),
-            'number': int(replica.number),
-            'path': str(replica.path),
-            'status': int(replica.status)
-        }]
-    print(data)
-
+    try:
+        sess = create_session()
+        obj = sess.data_objects.get(filename)
+        sess.cleanup()
+        data = {}
+        data['replicas'] = []
+        for replica in obj.replicas:
+            data['replicas'] += [{
+                'resource_name': str(replica.resource_name),
+                'number': int(replica.number),
+                'path': str(replica.path),
+                'status': int(replica.status)
+            }]
+        print(data)
+    except (CollectionDoesNotExist, DataObjectDoesNotExist):
+        return jsonify(dict(error="No replica found for %s"%filename)), 404
     return jsonify(data)
+
